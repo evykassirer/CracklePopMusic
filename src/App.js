135fce591fa41d1345d19b623497db4809ed3fbe
evykassirer <evy.kassirer@gmail.com>
2018-12-29 14:47:21 -0800

    omg it works!! mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import PitchPlayer from './PitchPlayer.js'
 
 class App extends Component {
+  state = {
+    number: 1
+  }
+
+  componentDidMount() {
+    setTimeout(this.incrementNumber, 1000)
+  }
+
+  incrementNumber = () => {
+    const number = this.state.number
+    if (number < 100) {
+      this.setState({
+        number: number + 1
+      })
+      setTimeout(this.incrementNumber, 1000)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Hello world!
+            <PitchPlayer number={this.state.number}/>
           </p>
         </header>
       </div>
