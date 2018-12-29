af52fa014a691eb831d407e8a82a866d70da8bbe
evykassirer <evy.kassirer@gmail.com>
2018-12-29 14:54:36 -0800

    Updates

---
+++ precache-manifest.366a3f38527669ab78f456ac0c67e391.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9826d1698a631a6c49be",
+    "url": "/crackle-pop-music/static/js/runtime~main.9826d169.js"
+  },
+  {
+    "revision": "1624953c75e52af80bbd",
+    "url": "/crackle-pop-music/static/js/main.1624953c.chunk.js"
+  },
+  {
+    "revision": "5430916698699166d24b",
+    "url": "/crackle-pop-music/static/js/1.54309166.chunk.js"
+  },
+  {
+    "revision": "1624953c75e52af80bbd",
+    "url": "/crackle-pop-music/static/css/main.81d96b6c.chunk.css"
+  },
+  {
+    "revision": "1ca39b4370ca3c352d6bcabcb31d1278",
+    "url": "/crackle-pop-music/index.html"
+  }
+];
